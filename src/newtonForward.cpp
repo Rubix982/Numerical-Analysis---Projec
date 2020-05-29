cbedf0faf45d32c014dc83835c2c85ee839b68a8
Saif Ul Islam <saifulislam84210@gmail.com>
2020-05-29 20:30:45 +0500

    newtonForward - Testing with measurements

--- src/newtonForward.cpp
+++ src/newtonForward.cpp
@@ -24,6 +24,7 @@
 #include <ctime>
 #include <sstream>
 #include <pthread.h>
+#include <cmath>
 
 //* ### TYPDEFs
 typedef unsigned long long ull;
@@ -53,6 +54,34 @@ getInputFromFile(void * dummy);
 */
 static inline void *
 getInputFromUser(void * dummy);
+
+/*
+*   @params void - no input required
+*   @returns void - data will be stored in inputBuffer
+*           which is a global variable
+*/
+static inline void
+calculateForwardDifferences(void);
+
+/*
+*   @params ull n - n should be greater than k
+*           ull k - k should be less than n
+*           Both should be positive, but it's unsigned already.
+*           Large positive values may produce overflow
+*   @returns ull - final result
+*
+*/
+static inline ull
+Choose(ull n, ull k);
+
+/*
+*   @params ull n - n is any positive number
+*           Large positive values may produce overflow
+*   @returns ull - final result
+*
+*/
+static inline ull
+factorial(ull n);
 
 /*
 *   @params void - no input required
@@ -103,7 +132,8 @@ int main(int argc, char const *argv[])
 
     getInput();
 
-    // display();
+    calculateForwardDifferences();
+    display();
 
     throwOutput();
 
@@ -188,6 +218,78 @@ getInputFromUser(void * dummy)
         " as " << X_input << "\n";
 
     return nullptr;
+}
+
+static inline void
+calculateForwardDifferences(void)
+{
+
+    /// Calculate the respective values of F
+    for ( ull i = 1 ; i < numOfInputs ; ++i )
+        for ( ull j = 1; j <= i ; j++ )
+            F[i][j] = (F[i][j-1] - F[i-1][j-1]) / (X[i] - X[i-j]);
+
+    /*
+        The equation below requires the use of's'. The book uses 's' without
+        clarifying why it exists and for what intents, exactly.
+
+        However, the below excerpt from page 126 of 'Numerical Analysis', by
+        Richard L.Burden, Douglas J.Faires, Annette M.Burden, 10E, provides
+        some clarification here,
+
+        "
+        Newton's divided-difference formula can be expressed in a simplified
+        form when the nodes are arranged consecutively with equal spacing. In
+        this case, we introduce the notation h= X(i+1) - X(i), for each
+        i = 0, 1, ... , n-1 and let X = X(0) + s*h. Then the difference X - X(i)
+        is X - X(i) = (s - i)*h. So, Eq(3.10) becomes
+
+            Pn(X) = ...
+        "
+
+        Last part omitted since not needed to explain this part
+
+        Since,
+            => X = X(0) + s*h
+            => X - X(0) = s*h
+            => s = ( X - X(0) ) / h --------------- ( A )
+
+        Where h is the value of the equal difference.
+        It is not guranted that the result of A is always an integer, thus
+        the final result will be ceiled in code
+
+    */
+
+    // Thus
+    ull s = ull(ceil(( X_input - X[0] ) / ( X[1] - X[0] )));
+    // std::cout << s << " " << (( X_input - X[0] ) / ( X[1] - X[0] ))  << "\n";
+
+    ld sumResult = 0.0f;
+    for ( ull k = 1 ; k < numOfInputs ; ++k )
+    {
+        ull temp = Choose(s, k);
+        std::cout << temp << " " << F[k][k] << "\n";
+        sumResult += (temp * F[k][k]);
+    }
+
+    Pn_X = F[0][0] + sumResult;
+}
+
+static inline ull
+Choose(ull n, ull k)
+{
+    if ( k == 0 or n == 0 or k == n )/* base case */
+        return 1u;
+    else                            /* recursive case */
+        return Choose(n-1, k-1) +   /* take */
+                Choose(n-1, k);     /* not take */
+}
+
+static inline ull
+factorial(ull n)
+{
+    if (n <= 1) return 1;                   /* base case */
+    else        return n*factorial(n-1);    /* recursive case */
 }
 
 static inline void
